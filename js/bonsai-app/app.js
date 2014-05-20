c76e0cdfffdbb444b26e2e943b014884aee0bac3
Martijn Veenstra <martijn@productplus.nl>
2014-05-20 16:58:36 +0200

    updated

--- js/bonsai-app/app.js
+++ js/bonsai-app/app.js
@@ -15,4 +15,3 @@ app.config(['$routeProvider',
         });
     }
 ]);
-
--- js/bonsai-app/controllers/detailsController.js
+++ js/bonsai-app/controllers/detailsController.js
@@ -1,5 +1,16 @@
-app.controller("detailsController", function($scope, $http) {
+app.controller("detailsController", function($scope, $http, bonsaiService, $routeParams) {
 
+    $scope.treeId = $routeParams.bonsaiId;
 
+    bonsaiService.get(function(data) {
+        $scope.tree = data.bomen;
+
+        angular.forEach($scope.tree, function(val, index) {
+            if (val.id == $routeParams.bonsaiId) {
+                $scope.name = val.name;
+
+            }
+        });
+    });
 
 });
